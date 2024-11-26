e6b942632e563715a2a5e8987a06c3bc9accdfbf
diosoid <reta_pablo_@hotmail.com>
2024-11-26 18:12:31 -0300

    Clase 0 70295

---
+++ src/models/userModel.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }
+
+})
+
+export default mongoose.model('user', userSchema)
---
+++ src/routes/userRouter.js
@@ -0,0 +1,62 @@
+import { Router } from "express";
+import userModel from "../models/userModel.js";
+
+const router = Router();
+router.get('/', async (req, res) => {
+    try {
+         const result = await userModel.find({})
+         res.send({status: 'success', payload: result})
+    } catch (error) {
+        res.status(400).send({status: 'error', message:error.message});  
+    }
+})
+
+router.post('/', async (req, res) => {
+    try {
+
+        const {name, age, email} = req.body
+        const newUser = {
+            name,
+            age,
+            email
+        }
+        const result = await userModel.create(newUser)
+        res.send({status: 'User Created', payload: result})
+
+    } catch (error) {    
+        res.status(400).send({status: 'error', message:error.message});   
+    }
+})
+
+router.patch('/:id', async (req, res) => {
+    try {
+        const {id} = req.params
+
+        const {name, age, email} = req.body
+        const data = {
+            name,
+            age,
+            email
+        }
+        
+        const result = await userModel.updateOne({_id: id}, data)
+        res.send({status: 'Data updated', payload: result})
+
+    } catch (error) {    
+        res.status(400).send({status: 'error', message:error.message});   
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const {id} = req.params
+
+        const result = await userModel.deleteOne({_id: id})
+        res.send({status: 'success', payload: result})
+
+    } catch (error) {    
+        res.status(400).send({status: 'error', message:error.message});   
+    }
+})
+
+export default router
